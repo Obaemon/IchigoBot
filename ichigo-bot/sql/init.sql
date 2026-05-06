434064c3b48ef5c0182388d1db3343e0f575205d
obaemon <obahajime1291@icloud.com>
2026-05-06 15:54:23 +0900

    DB定義書に合わせてSQL修正

--- ichigo-bot/sql/init.sql
+++ ichigo-bot/sql/init.sql
@@ -1,18 +1,37 @@
-CREATE TABLE IF NOT EXISTS events (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
+CREATE TABLE IF NOT EXISTS ichigotsumi (
+    id INTEGER PRIMARY KEY,
+    head_user_id TEXT NOT NULL,
     start_date TEXT NOT NULL,
     end_date TEXT
 );
 
-CREATE TABLE IF NOT EXISTS tanka (
+CREATE TABLE IF NOT EXISTS post (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT NOT NULL,
-    user_id TEXT,
-    user_name TEXT,
-    created_at TEXT,
-    event_id INTEGER,
-    message_id TEXT,
+    user_id TEXT NOT NULL,
+    user_name TEXT NOT NULL,
+    message TEXT NOT NULL,
+    post_date TEXT NOT NULL,
+    ichigotsumi_id INTEGER NOT NULL,
+    message_id TEXT NOT NULL,
     previous_message_id TEXT,
-    FOREIGN KEY (event_id) REFERENCES events(id)
+    FOREIGN KEY (ichigotsumi_id) REFERENCES ichigotsumi(id)
+);
+
+CREATE TABLE IF NOT EXISTS leaders (
+    user_id TEXT PRIMARY KEY,
+    user_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS topics (
+    ichigotsumi_id INTEGER NOT NULL,
+    open_date TEXT NOT NULL,
+    close_date TEXT NOT NULL,
+    leader_user_id TEXT NOT NULL,
+    contents TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS words (
+    word TEXT NOT NULL,
+    season INTEGER,
+    month INTEGER
 );
